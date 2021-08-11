824bd932b64e17d0a38111acab11001a6185d907
Linkon Miyan <rudlinkon@gmail.com>
2021-08-11 23:23:39 +0600

    Added Dashboard new layout design

--- wp-content/plugins/myaliceai/includes/myalice-dashboard-inline-styles.php
+++ wp-content/plugins/myaliceai/includes/myalice-dashboard-inline-styles.php
@@ -5,6 +5,7 @@ defined( 'ABSPATH' ) || die;
 // Admin Internal Style
 add_action( 'admin_head', function () { ?>
     <style>
+        /* Alice Admin Menu */
         #adminmenu .toplevel_page_myalice_dashboard > .wp-menu-image > img {
             padding: 5px 0 0 0;
             opacity: 1;
@@ -23,6 +24,7 @@ add_action( 'admin_head', function () { ?>
             filter: grayscale(0);
         }
 
+        /* Alice Deactivation Feedback Modal */
         #alice-feedback-modal {
             position: fixed;
             top: 0;
@@ -149,8 +151,200 @@ add_action( 'admin_head', function () { ?>
             margin-right: 5px;
         }
 
-        .alice-dashboard-tab-content th.disabled {
-            opacity: .6;
+        /* Alice Dashboard */
+        #alice-dashboard * {
+            box-sizing: border-box;
+        }
+
+        #alice-dashboard h1,
+        #alice-dashboard h2,
+        #alice-dashboard h3,
+        #alice-dashboard h4,
+        #alice-dashboard h5,
+        #alice-dashboard h6,
+        #alice-dashboard p,
+        #alice-dashboard ul,
+        #alice-dashboard ol,
+        #alice-dashboard li,
+        #alice-dashboard a,
+        #alice-dashboard button {
+            margin: 0;
+            padding: 0;
+        }
+
+        #alice-dashboard .alice-container {
+            max-width: 956px;
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        #alice-dashboard .alice-btn {
+            background: #04B25F;
+            color: #fff;
+            border-radius: 6px;
+            font-size: 16px;
+            line-height: 40px;
+            border: 1px solid transparent;
+            padding: 0 17px;
+            text-decoration: none;
+            transition: .3s;
+            display: inline-block;
+            cursor: pointer;
+        }
+
+        #alice-dashboard .alice-btn:hover {
+            background: #039952;
+            color: #fff;
+        }
+
+        #alice-dashboard .alice-btn.white-btn {
+            background: #fff;
+            border: 1px solid #D1D5DB;
+            color: #374151;
+            box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+        }
+
+        #alice-dashboard .alice-btn.white-btn:hover {
+            box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+            color: #000;
+        }
+
+        #alice-dashboard .alice-input-field {
+            line-height: 40px;
+            border: 1px solid #D1D5DB;
+            border-radius: 6px;
+            font-size: 14px;
+            width: 400px;
+            padding: 0 12px;
+            color: #6B7280;
+        }
+
+        #alice-dashboard .alice-dashboard-header {
+            background: #fff;
+            padding: 20px 0;
+        }
+
+        #alice-dashboard  .alice-welcome-section {
+            margin-top: 24px;
+            padding: 12px 0;
+        }
+
+        #alice-dashboard  .alice-welcome-section h3 {
+            font-size: 24px;
+            line-height: 32px;
+            color: #111827;
+            margin-left: 12px;
+            font-weight: 700;
+        }
+
+        #alice-dashboard .alice-welcome-section .alice-container {
+            justify-content: initial;
+        }
+
+        #alice-dashboard .alice-registration-guide {
+            margin-top: 12px;
+        }
+
+        #alice-dashboard .alice-registration-guide .alice-container {
+            background: #fff;
+            border-radius: 8px;
+            padding: 38px 24px;
+        }
+
+        #alice-dashboard .alice-text-content {
+            border-left: 4px solid #04B25F;
+            padding-left: 12px;
+            width: 50%;
+        }
+
+        #alice-dashboard .alice-text-content h4 {
+            font-size: 18px;
+            color: #111827;
+            margin-bottom: 10px;
+        }
+
+        #alice-dashboard .alice-text-content p {
+            font-size: 14px;
+            color: #6B7280;
+        }
+
+        #alice-dashboard .alice-btn-content .alice-btn + .alice-btn {
+            margin-left: 12px;
+        }
+
+        #alice-dashboard .alice-marketplace-connect-section > .alice-container,
+        #alice-dashboard .alice-plugin-key-section > .alice-container {
+            background: #fff;
+            border-radius: 8px;
+            padding: 24px;
+            margin-top: 24px;
+        }
+
+        #alice-dashboard .alice-marketplace-connect-section > .alice-container .alice-container {
+            padding-bottom: 8px;
+        }
+
+        #alice-dashboard .alice-marketplace-connect-section .alice-text-content,
+        #alice-dashboard .alice-plugin-key-section .alice-text-content {
+            border: none;
+            padding: 0
+        }
+
+        #alice-dashboard .alice-marketplace-connect-section ol {
+            border-top: 1px solid #D1D5DB;
+            padding: 15px 0 0 15px;
+        }
+
+        #alice-dashboard .alice-marketplace-connect-section ol li {
+            font-size: 14px;
+            line-height: 22px;
+            color: #111827;
+        }
+
+        #alice-dashboard .alice-plugin-key-section .alice-text-content {
+            width: 70%;
+        }
+
+        #alice-dashboard .alice-plugin-key {
+            margin-top: 20px;
+        }
+
+        #alice-dashboard .alice-plugin-key-section .alice-input-group > * + * {
+            margin-left: 12px;
+        }
+
+        #alice-dashboard .alice-plugin-key input.alice-input-field {
+            background: #E5E7EB;
+        }
+
+        #alice-dashboard .alice-plugin-key [type="submit"] {
+            display: none;
+        }
+
+        #alice-dashboard .alice-plugin-key .alice-edit-btn {
+            display: inline-block;
+        }
+
+        #alice-dashboard .alice-plugin-key .alice-cancel-btn {
+            display: none;
+        }
+
+        #alice-dashboard .alice-plugin-key .alice-active-editing input.alice-input-field {
+            background: #FFFFFF;
+        }
+
+        #alice-dashboard .alice-plugin-key-section .alice-active-editing [type="submit"] {
+            display: inline-block;
+        }
+
+        #alice-dashboard .alice-plugin-key .alice-active-editing .alice-edit-btn {
+            display: none;
+        }
+
+        #alice-dashboard .alice-plugin-key .alice-active-editing .alice-cancel-btn {
+            display: inline-block;
         }
     </style>
 	<?php
--- wp-content/plugins/myaliceai/includes/myalice-dashboard-templates-and-scripts.php
+++ wp-content/plugins/myaliceai/includes/myalice-dashboard-templates-and-scripts.php
@@ -78,7 +78,7 @@ add_action( 'admin_footer', function () { ?>
 
                 var deactivate_url = $('#deactivate-myaliceai').attr('href');
                 location.href = '<?php echo admin_url(); ?>' + deactivate_url;
-            }).on('submit', '.alice-api-connect-tab form', function (e) {
+            }).on('submit', '.alice-plugin-key form', function (e) {
                 e.preventDefault();
 
                 var $form = $(this),
@@ -92,17 +92,15 @@ add_action( 'admin_footer', function () { ?>
                     spinner.removeClass('is-active');
 
                     var notice_area = $('.myalice-notice-area'),
-                        platform_id = $('#alice-platform-id'),
-                        primary_id = $('#alice-primary-id');
+                        input_group = $('.alice-input-group'),
+                        input_field = $('#alice-plugin-key');
 
                     if (response.success) {
                         notice_area.prepend(`<div class="updated"><p>${response.data.message}</p></div>`);
-                        platform_id.val(response.data.platform_id);
-                        primary_id.val(response.data.primary_id);
+                        input_group.removeClass('alice-active-editing');
+                        input_field.attr('readonly', 'readonly');
                     } else {
                         notice_area.prepend(`<div class="error"><p>${response.data.message}</p></div>`);
-                        platform_id.val('');
-                        primary_id.val('');
                     }
 
                     setTimeout(function () {
@@ -118,6 +116,19 @@ add_action( 'admin_footer', function () { ?>
                         notice_wrap.remove();
                     }
                 });
+            }).on('click', '.alice-edit-btn, .alice-cancel-btn', function (e) {
+                e.preventDefault();
+
+                var $this = $(this),
+                    input_group = $('.alice-input-group'),
+                    input_field = $('#alice-plugin-key');
+
+                input_group.toggleClass('alice-active-editing');
+                if ($this.hasClass('alice-cancel-btn')) {
+                    input_field.attr('readonly', 'readonly');
+                } else {
+                    input_field.removeAttr('readonly');
+                }
             });
         })(jQuery);
     </script>
