2770c803912c2311632fee21b0b5d5605d73a499
Linkon Miyan <rudlinkon@gmail.com>
2021-08-13 00:49:46 +0600

    Fix cookie path

--- wp-content/plugins/myaliceai/js/script.js
+++ wp-content/plugins/myaliceai/js/script.js
@@ -3,4 +3,4 @@ let aliceDataString = localStorage.getItem("persist:inconnect:webchat:sdk");
 let aliceDataObj = JSON.parse(aliceDataString);
 let aliceDataChatBotObj = JSON.parse(aliceDataObj.chatbot);
 let aliceCustomerId = aliceDataChatBotObj.customerID;
-document.cookie = "aliceCustomerId = " + aliceCustomerId;
+document.cookie = "aliceCustomerId = " + aliceCustomerId + ";path=/;";
