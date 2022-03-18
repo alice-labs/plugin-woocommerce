735a108d9b15cfabac4e3016d8fc66e2677a31e8
Linkon Miyan <rudlinkon@gmail.com>
2022-03-18 11:03:02 +0600

    chatbox will be controlled from settings

--- wp-content/plugins/myaliceai/includes/myalice-enqueue-scripts.php
+++ wp-content/plugins/myaliceai/includes/myalice-enqueue-scripts.php
@@ -2,6 +2,8 @@
 // Direct file access is disallowed
 defined( 'ABSPATH' ) || die;
 
+global $myalice_settings;
+
 // Insert JS code in Footer
 function alice_chatbot_script_callback() { ?>
     <script type="text/javascript">
@@ -35,5 +37,7 @@ if ( MYALICE_API_OK ) {
 		wp_enqueue_script( 'alice-script', ALICE_JS_PATH . 'script.js', [ 'jquery' ], ALICE_VERSION, false );
 	} );
 
-	add_action( 'wp_footer', 'alice_chatbot_script_callback' );
+	if ( $myalice_settings['allow_chat_user_only'] === 0 || ( $myalice_settings['allow_chat_user_only'] === 1 && is_user_logged_in() ) ) {
+		add_action( 'wp_footer', 'alice_chatbot_script_callback' );
+	}
 }
