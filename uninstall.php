3bfff2a330b8b6136f8027a015f408e4d72558b1
Linkon Miyan <rudlinkon@gmail.com>
2022-04-20 00:07:50 +0600

    delete settings data when uninstall the plugin

--- uninstall.php
+++ uninstall.php
@@ -7,6 +7,7 @@
 defined( 'WP_UNINSTALL_PLUGIN' ) || die;
 
 delete_option( 'myaliceai_api_data' );
+delete_option( 'myaliceai_settings' );
 delete_option( 'myaliceai_plugin_status' );
 delete_option( 'myaliceai_review_notice_time' );
 
