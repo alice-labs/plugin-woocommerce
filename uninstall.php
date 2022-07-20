d2c1aeca8d5c1342e4b9bd1ded18932c297c2ec5
Linkon Miyan <rudlinkon@gmail.com>
2022-07-21 00:26:35 +0600

    fixed uninstallation api not working

--- uninstall.php
+++ uninstall.php
@@ -12,8 +12,11 @@ delete_option( 'myaliceai_plugin_status' );
 delete_option( 'myaliceai_review_notice_time' );
 delete_option( 'myaliceai_wc_auth' );
 
+$api_data = get_option( 'myaliceai_api_data' );
+$api_data = wp_parse_args( $api_data, [ 'api_token' => '' ] );
+
 // Plugin remove API
-$alice_api_url = MYALICE_API_URL . 'remove-ecommerce-plugin?api_token=' . MYALICE_API_TOKEN;
+$alice_api_url = 'https://live-v3.getalice.ai/api/ecommerce/plugins/remove-ecommerce-plugin?api_token=' . $api_data['api_token'];
 wp_remote_post( $alice_api_url, array(
 		'method'  => 'POST',
 		'timeout' => 45,
