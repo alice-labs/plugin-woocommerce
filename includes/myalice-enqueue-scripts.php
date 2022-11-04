26edba1f200b76ae2ca1cf5e3f12436bf983b2fa
Linkon Miyan <rudlinkon@gmail.com>
2022-11-04 22:18:24 +0600

    added minified injected js

--- includes/myalice-enqueue-scripts.php
+++ includes/myalice-enqueue-scripts.php
@@ -6,27 +6,7 @@ global $myalice_settings;
 
 // Insert JS code in Footer
 function alice_chatbot_script_callback() { ?>
-    <script>
-        !function () {
-            var e = document.createElement("div");
-            e.id = "myAliceWebChat";
-            var t = document.createElement("script");
-            t.type = "text/javascript",
-            t.async = !0,
-            t.src = "https://livechat.myalice.ai/index.js";
-            var a = document.body.getElementsByTagName("script");
-            (a = a[a.length - 1]).parentNode.insertBefore(t, a),
-                a.parentNode.insertBefore(e, a),
-                t.addEventListener("load", function () {
-                MyAliceWebChat.init({
-                    selector: '#myAliceWebChat',
-                    platformId: '<?php echo esc_js( MYALICE_PLATFORM_ID ); ?>',
-                    primaryId: '<?php echo esc_js( MYALICE_PRIMARY_ID ); ?>',
-                    token: '<?php echo esc_js( MYALICE_API_TOKEN ); ?>'
-                })
-            })
-        }();
-    </script>
+    <script>!function(){var e=document.createElement("div");e.id="myAliceWebChat";var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://livechat.myalice.ai/index.js";var a=document.body.getElementsByTagName("script");(a=a[a.length-1]).parentNode.insertBefore(t,a),a.parentNode.insertBefore(e,a),t.addEventListener("load",function(){MyAliceWebChat.init({selector:"#myAliceWebChat",platformId:"<?php echo esc_js( MYALICE_PLATFORM_ID ); ?>",primaryId:"<?php echo esc_js( MYALICE_PRIMARY_ID ); ?>",token:"<?php echo esc_js( MYALICE_API_TOKEN ); ?>"})})}();</script>
 	<?php
 }
 
