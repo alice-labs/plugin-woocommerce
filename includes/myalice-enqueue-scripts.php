0427ffaba696649f98fbc6882225b27b96ebfe4c
Linkon Miyan <rudlinkon@gmail.com>
2022-11-07 21:24:33 +0600

    update injected script condition

--- includes/myalice-enqueue-scripts.php
+++ includes/myalice-enqueue-scripts.php
@@ -6,11 +6,7 @@ global $myalice_settings;
 
 // Insert JS code in Footer
 function alice_chatbot_script_callback() {
-	if ( ! get_option( 'myalice_is_needed_migration' ) ) {
-		?>
-        <script>!function(){var e=document.createElement("div");e.id="myAliceWebChat";var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://livechat.myalice.ai/index.js";var a=document.body.getElementsByTagName("script");(a=a[a.length-1]).parentNode.insertBefore(t,a),a.parentNode.insertBefore(e,a),t.addEventListener("load",function(){MyAliceWebChat.init({selector:"#myAliceWebChat",platformId:"<?php echo esc_js( MYALICE_PLATFORM_ID ); ?>",primaryId:"<?php echo esc_js( MYALICE_PRIMARY_ID ); ?>",token:"<?php echo esc_js( MYALICE_API_TOKEN ); ?>"})})}();</script>
-		<?php
-	} else {
+	if ( get_option( 'myalice_is_needed_migration' ) ) {
 		?>
         <script type="text/javascript">
             (function () {
@@ -34,6 +30,10 @@ function alice_chatbot_script_callback() {
                 });
             })();
         </script>
+		<?php
+	} else {
+		?>
+        <script>!function(){var e=document.createElement("div");e.id="myAliceWebChat";var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://livechat.myalice.ai/index.js";var a=document.body.getElementsByTagName("script");(a=a[a.length-1]).parentNode.insertBefore(t,a),a.parentNode.insertBefore(e,a),t.addEventListener("load",function(){MyAliceWebChat.init({selector:"#myAliceWebChat",platformId:"<?php echo esc_js( MYALICE_PLATFORM_ID ); ?>",primaryId:"<?php echo esc_js( MYALICE_PRIMARY_ID ); ?>",token:"<?php echo esc_js( MYALICE_API_TOKEN ); ?>"})})}();</script>
 		<?php
 	}
 }
