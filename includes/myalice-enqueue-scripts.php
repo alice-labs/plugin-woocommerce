aad1960897224352590dab3f4968296be9f013c0
Linkon Miyan <rudlinkon@gmail.com>
2022-11-07 20:13:57 +0600

    use old script if needed migration otherwise load new script

--- includes/myalice-enqueue-scripts.php
+++ includes/myalice-enqueue-scripts.php
@@ -5,9 +5,37 @@ defined( 'ABSPATH' ) || die;
 global $myalice_settings;
 
 // Insert JS code in Footer
-function alice_chatbot_script_callback() { ?>
-    <script>!function(){var e=document.createElement("div");e.id="myAliceWebChat";var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://livechat.myalice.ai/index.js";var a=document.body.getElementsByTagName("script");(a=a[a.length-1]).parentNode.insertBefore(t,a),a.parentNode.insertBefore(e,a),t.addEventListener("load",function(){MyAliceWebChat.init({selector:"#myAliceWebChat",platformId:"<?php echo esc_js( MYALICE_PLATFORM_ID ); ?>",primaryId:"<?php echo esc_js( MYALICE_PRIMARY_ID ); ?>",token:"<?php echo esc_js( MYALICE_API_TOKEN ); ?>"})})}();</script>
-	<?php
+function alice_chatbot_script_callback() {
+	if ( ! get_option( 'myalice_is_needed_migration' ) ) {
+		?>
+        <script>!function(){var e=document.createElement("div");e.id="myAliceWebChat";var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://livechat.myalice.ai/index.js";var a=document.body.getElementsByTagName("script");(a=a[a.length-1]).parentNode.insertBefore(t,a),a.parentNode.insertBefore(e,a),t.addEventListener("load",function(){MyAliceWebChat.init({selector:"#myAliceWebChat",platformId:"<?php echo esc_js( MYALICE_PLATFORM_ID ); ?>",primaryId:"<?php echo esc_js( MYALICE_PRIMARY_ID ); ?>",token:"<?php echo esc_js( MYALICE_API_TOKEN ); ?>"})})}();</script>
+		<?php
+	} else {
+		?>
+        <script type="text/javascript">
+            (function () {
+                var div = document.createElement('div');
+                div.id = 'icWebChat';
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.async = true;
+                script.src = 'https://webchat.getalice.ai/index.js';
+                var lel = document.body.getElementsByTagName('script');
+                var el = lel[lel.length - 1];
+                el.parentNode.insertBefore(script, el);
+                el.parentNode.insertBefore(div, el);
+                script.addEventListener('load', function () {
+                    ICWebChat.init({
+                        selector: '#icWebChat',
+                        platformId: '<?php echo esc_js( MYALICE_PLATFORM_ID ); ?>',
+                        primaryId: '<?php echo esc_js( MYALICE_PRIMARY_ID ); ?>',
+                        token: '<?php echo esc_js( MYALICE_API_TOKEN ); ?>'
+                    });
+                });
+            })();
+        </script>
+		<?php
+	}
 }
 
 //JS File will be enqueued if valid API are given.
--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
 // Alice Customer ID
-var promise = new Promise(function (resolve, reject) {
-    var interval = setInterval(function () {
-        var aliceDataString = localStorage.getItem("persist:myalice:webchat:sdk"),
-            aliceDataObj;
+if (myaliceai.is_needed_migration) {
+    var promise = new Promise(function (resolve, reject) {
+        var interval = setInterval(function () {
+            var aliceDataString = localStorage.getItem("persist:inconnect:webchat:sdk"),
+                aliceDataObj;
 
-        if (aliceDataString !== null && (aliceDataObj = JSON.parse(aliceDataString))) {
-            var aliceDataChatBotObj = JSON.parse(aliceDataObj.livechat);
+            if (aliceDataString !== null && (aliceDataObj = JSON.parse(aliceDataString))) {
+                var aliceDataChatBotObj = JSON.parse(aliceDataObj.chatbot);
 
-            if (aliceDataChatBotObj.customerId !== null) {
-                clearInterval(interval);
+                if (aliceDataChatBotObj.customerID !== null) {
+                    clearInterval(interval);
 
-                resolve(aliceDataChatBotObj.customerId);
+                    resolve(aliceDataChatBotObj.customerID);
+                }
             }
-        }
-    }, 100);
-});
+        }, 100);
+    });
+} else {
+    var promise = new Promise(function (resolve, reject) {
+        var interval = setInterval(function () {
+            var aliceDataString = localStorage.getItem("persist:myalice:webchat:sdk"),
+                aliceDataObj;
+
+            if (aliceDataString !== null && (aliceDataObj = JSON.parse(aliceDataString))) {
+                var aliceDataChatBotObj = JSON.parse(aliceDataObj.livechat);
+
+                if (aliceDataChatBotObj.customerId !== null) {
+                    clearInterval(interval);
+
+                    resolve(aliceDataChatBotObj.customerId);
+                }
+            }
+        }, 100);
+    });
+}
 
 promise.then(function (aliceCustomerId) {
     document.cookie = "aliceCustomerId = " + aliceCustomerId + ";path=/;";
