43b5723f79e603324f8826503ebdff24742c5655
Linkon Miyan <rudlinkon@gmail.com>
2022-11-17 21:22:06 +0600

    delete migration notice data during the plugin deletion.

--- uninstall.php
+++ uninstall.php
@@ -14,6 +14,8 @@ delete_option( 'myaliceai_settings' );
 delete_option( 'myaliceai_plugin_status' );
 delete_option( 'myaliceai_review_notice_time' );
 delete_option( 'myaliceai_wc_auth' );
+delete_option( 'myaliceai_is_needed_migration' );
+delete_option( 'myaliceai_customization_notice_dismiss' );
 
 // Plugin remove API
 $alice_api_url = 'https://api.myalice.ai/stable/ecommerce/remove-woocommerce-integration?api_token=' . $api_data['api_token'];
