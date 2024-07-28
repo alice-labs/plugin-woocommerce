251ffae49c29b9cc2bd522d864549267dae35e38
Linkon Miyan <rudlinkon@gmail.com>
2024-07-28 11:32:44 +0600

    added HPOS support

--- myaliceai.php
+++ myaliceai.php
@@ -119,3 +119,14 @@ add_action( 'plugins_loaded', function () {
 	require ALICE_INC_PATH . '/myalice-hooks.php';
 	require ALICE_INC_PATH . '/myalice-hooks-callback.php';
 } );
+
+/**
+ * WooCommerce HPOS Support
+ *
+ * @since v2.5.1
+ */
+add_action( 'before_woocommerce_init', function() {
+	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+	}
+} );
